9a0f6920b533e1cb047a32498cfc1e53a8dd25da
Felix Chern <fchern@google.com>
2016-08-04 20:07:29 -0700

    Update mathjax setup

--- publish.el
+++ publish.el
@@ -11,6 +11,16 @@
          :publishing-directory "./public_html/"
          :publishing-function org-publish-attachment
          :recursive t)))
+;; The defaults mathjax is old and slow
+(setf org-html-mathjax-options
+      '((path "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML")
+        (scale "100")
+        (align "center")
+        (indent "2em")
+        (mathml nil)))
+(setf org-html-mathjax-template
+      "<script type=\"text/javascript\" src=\"%PATH\"></script>")
+
 (org-publish-all)
 
 ;; We can use the :completion-function to publish to github page
